dc46bc6597895c14f54f835f8c6882766b73935c
sameera222 <sameeraanjumh@gmail.com>
2024-01-17 13:57:50 +0530

    add card details to the car showcase

--- types/index.ts
+++ types/index.ts
@@ -1,12 +1,29 @@
 import { MouseEventHandler } from "react";
 
 export interface CustomButtonProps {
-    title: string;
-    containerStyles?: string;
-    handleClick?: MouseEventHandler<HTMLButtonElement>;
-    btnType?: 'button' | 'submit';
+  title: string;
+  textStyles?:string;
+  rightIcon?:string;
+  containerStyles?: string;
+  handleClick?: MouseEventHandler<HTMLButtonElement>;
+  btnType?: "button" | "submit";
 }
-export interface SearchManufactureProps{
-    manufacture: string;
-    setManufacture: (manufacture: string) => void;
+export interface SearchManufactureProps {
+  manufacture: string;
+
+  setManufacture: (manufacture: string) => void;
+}
+export interface CarProps {
+  city_mpg: number;
+  class: string;
+  combination_mpg: number;
+  cylinders: number;
+  displacement: number;
+  drive: string;
+  fuel_type: string;
+  highway_mpg: number;
+  make: string;
+  model: string;
+  transmission: string;
+  year: number;
 }
